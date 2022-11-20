b0792d66377e28437d0d2f55ebd015d9b565a432
yanakhorolska <yana.khorolska@protonmail.com>
2022-11-20 17:10:10 +0100

    Update AppMenu.styled.jsx

--- src/components/AppMenu/AppMenu.styled.jsx
+++ src/components/AppMenu/AppMenu.styled.jsx
@@ -2,17 +2,24 @@ import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
 export const NavItem = styled(NavLink)`
+  padding: 10px;
+  text-decoration: none;
+  color: black;
+  font-size: 25px;
   &.active {
     color: green;
   }
 `;
 export const Header = styled('div')`
+  background-color: black;
   height: 50px;
   padding: 0;
   margin: 0;
   border-bottom: 1px solid black;
-  box-shadow: 0px 1px 8px black;
+
   width: 100%;
 `;
 
-export const NavList = styled('ul')``;
+export const NavList = styled('ul')`
+  display: flex;
+`;
